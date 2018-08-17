c067054ed009bb8758567b1b1d63c4c73810cdb9
calvon <calvongchi@gmail.com>
2018-08-18 04:10:19 +1000

    FS: added msg sending to Odroid

--- libraries/AP_FakeSensor/AP_FakeSensor.cpp
+++ libraries/AP_FakeSensor/AP_FakeSensor.cpp
@@ -16,20 +16,20 @@ void AP_FakeSensor::get_AHRS(AP_AHRS_View* ahrs)
 
 void AP_FakeSensor::init()
 {
-    uart = hal.uartC;  // using telem1 port
-    if (uart == nullptr)    {return;}
+    _uart = hal.uartC;  // using telem1 port
+    if (_uart == nullptr)    {return;}
 
-    uart->begin(ODROID_BAUDRATE);
+    _uart->begin(ODROID_BAUDRATE);
 
     gcs().send_text(MAV_SEVERITY_INFO, "Initialising fake sensor communication with Odroid...\n");
 }
 
 void AP_FakeSensor::update()
 {
-    if (uart == nullptr)    {return;}
+    if (_uart == nullptr)    {return;}
 
 
-    int16_t nbytes = uart->available();
+    int16_t nbytes = _uart->available();
 
     char d[4];      // temp buffer for 4 digits
 
@@ -37,7 +37,7 @@ void AP_FakeSensor::update()
 // position update
     while (nbytes-- > 0)
     {
-        char c = uart->read();
+        char c = _uart->read();
 
         // decode the message
         if (c == '#')   // end bit: start processing data
@@ -90,13 +90,62 @@ void AP_FakeSensor::update()
 // assign timestamp to data
     _data.ts = AP_HAL::millis();
 
-    gcs().send_text(MAV_SEVERITY_INFO, "roll: %f  %f %ld\n", _ahrs->roll*180/M_PI, _data.pitch*180/M_PI, _data.ts);
+    vector<unsigned char> msg;
+    msg = msg_encoder();
 
+    //gcs().send_text(MAV_SEVERITY_INFO, "msg %c, %d, %f", (char) msg[0], (int) ((msg[1]<<24|msg[2]<<16|msg[3]<<8|msg[4])*180/M_PI), _data.roll*1000);
     // signal to get another reading
-    uart->set_blocking_writes(false);
-    uart->write("abcd2345423");
-    uart->set_blocking_writes(true);
+    msg_sender(msg);
 
+
+}
+
+
+vector<unsigned char> AP_FakeSensor::msg_encoder()
+{
+//=================================
+//********  MSG FORMAT  ***********
+//=================================
+//  variables:
+//  roll, pitch, yaw are x1000 and sent as int
+//
+//  message:
+//  $roll pitch yaw ts#
+
+    vector<unsigned char> result;
+
+    int roll        = static_cast<int>(_data.roll*1000);
+    int pitch       = static_cast<int>(_data.pitch*1000);
+    int yaw         = static_cast<int>(_data.yaw*1000);
+    unsigned int ts = _data.ts;
+
+    result.push_back('$');
+    result = _int2byte(result, roll);
+    result = _int2byte(result, pitch);
+    result = _int2byte(result, yaw);
+    result = _int2byte(result, ts);
+
+    return result;
+}
+
+vector<unsigned char> AP_FakeSensor::_int2byte(vector<unsigned char> in, int value)
+{
+    in.push_back(value >> 24);
+    in.push_back(value >> 16);
+    in.push_back(value >>  8);
+    in.push_back(value      );
+    return in;
+}
+
+
+void AP_FakeSensor::msg_sender(vector<unsigned char> msg)
+{
+    _uart->set_blocking_writes(false);
+    for (int i=0; i < msg.size(); i++)
+    {
+        _uart->write(msg[i]);
+    }
+    _uart->set_blocking_writes(true);
 }
 
 FakeSensor_data AP_FakeSensor::get_data()
