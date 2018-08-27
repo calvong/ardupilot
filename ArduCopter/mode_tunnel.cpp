41087bf4927251a4eca6620411a6dceacff156c1
calvon <calvongchi@gmail.com>
2018-08-28 08:15:31 +1000

    BS: added mode transition throttle smoothing

--- ArduCopter/mode_tunnel.cpp
+++ ArduCopter/mode_tunnel.cpp
@@ -38,6 +38,7 @@ void Copter::ModeTunnel::run()
     if (!motors->armed() || !motors->get_interlock())
     {
         backstepping->reset_integral();
+        backstepping->reset_mode_switch();
     }
 
     motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
