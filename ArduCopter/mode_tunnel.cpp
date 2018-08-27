6307196785c46a4552e6b26e236e0ffc07677273
calvon <calvongchi@gmail.com>
2018-08-27 22:36:09 +1000

    MODE: remove unused sections in Tunnel mode

--- ArduCopter/mode_tunnel.cpp
+++ ArduCopter/mode_tunnel.cpp
@@ -17,20 +17,12 @@ bool Copter::ModeTunnel::init(bool ignore_checks)
     return true;
 }
 
-// althold_run - runs the althold controller
+// Tunnel_run - runs the backstepping controller
 // should be called at 100hz or more
 void Copter::ModeTunnel::run()
 {
-    AltHoldModeState althold_state;
-    float takeoff_climb_rate = 0.0f;
-
-    // initialize vertical speeds and acceleration
-    pos_control->set_speed_z(-get_pilot_speed_dn(), g.pilot_speed_up);
-    pos_control->set_accel_z(g.pilot_accel_z);
-
     // get gains for backstepping
     backstepping->get_gains(g.BS_y_k1,g.BS_y_k2,g.BS_y_k3,g.BS_z_k1,g.BS_z_k2,g.BS_z_k3);
-    backstepping->get_imax(g.BS_imax_y, g.BS_imax_z);
 
     // apply SIMPLE mode transform to pilot inputs
     update_simple_mode();
@@ -42,107 +34,25 @@ void Copter::ModeTunnel::run()
     // get pilot's desired yaw rate
     float target_yaw_rate = get_pilot_desired_yaw_rate(channel_yaw->get_control_in());
 
-    // get pilot desired climb rate
-    float target_climb_rate = get_pilot_desired_climb_rate(channel_throttle->get_control_in());
-    target_climb_rate = constrain_float(target_climb_rate, -get_pilot_speed_dn(), g.pilot_speed_up);
-
-    // Alt Hold State Machine Determination
-    if (!motors->armed() || !motors->get_interlock()) {
-        althold_state = AltHold_MotorStopped;
-    } else if (takeoff.running() || takeoff.triggered(target_climb_rate)) {
-        althold_state = AltHold_Takeoff;
-    } else if (!ap.auto_armed || ap.land_complete) {
-        althold_state = AltHold_Landed;
-    } else {
-        althold_state = AltHold_Flying;
+    // reset integral if on the ground
+    if (!motors->armed() || !motors->get_interlock())
+    {
+        backstepping->reset_integral();
     }
 
-    // Alt Hold State Machine
-    switch (althold_state) {
+    motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
 
-    case AltHold_MotorStopped:
+    // call attitude controller
+    attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
 
-        motors->set_desired_spool_state(AP_Motors::DESIRED_SHUT_DOWN);
-        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
-        attitude_control->reset_rate_controller_I_terms();
-        attitude_control->set_yaw_target_to_current_heading();
+    // call backstepping controller
+    backstepping->get_target_pos(0, g.BS_altd); // TODO
+    backstepping->pos_update();
 
-        pos_control->relax_alt_hold_controllers(0.0f);   // forces throttle output to go to zero
+    backstepping->update_alt_controller();
 
-        pos_control->update_z_controller();
+    // get control outputs
+    pos_sensor.read_controller(backstepping->perr, backstepping->get_u1());
+    pos_sensor.data.AC_alt_target = g.BS_altd;
 
-        // backstepping
-        backstepping->reset_integral();
-
-        break;
-
-    case AltHold_Takeoff:
-
-        // set motors to full range
-        motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
-
-        // initiate take-off
-        if (!takeoff.running()) {
-            takeoff.start(constrain_float(g.pilot_takeoff_alt,0.0f,1000.0f));
-            // indicate we are taking off
-            set_land_complete(false);
-            // clear i terms
-            set_throttle_takeoff();
-        }
-
-        // get take-off adjusted pilot and takeoff climb rates
-        takeoff.get_climb_rates(target_climb_rate, takeoff_climb_rate);
-
-        // get avoidance adjusted climb rate
-        target_climb_rate = get_avoidance_adjusted_climbrate(target_climb_rate);
-
-        // call attitude controller
-        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
-
-        // call position controller
-        pos_control->set_alt_target_from_climb_rate_ff(target_climb_rate, G_Dt, false);
-        pos_control->add_takeoff_climb_rate(takeoff_climb_rate, G_Dt);
-        pos_control->update_z_controller();
-        break;
-
-    case AltHold_Landed:
-        // set motors to spin-when-armed if throttle below deadzone, otherwise full range (but motors will only spin at min throttle)
-        if (target_climb_rate < 0.0f) {
-            motors->set_desired_spool_state(AP_Motors::DESIRED_SPIN_WHEN_ARMED);
-        } else {
-            motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
-        }
-
-
-        attitude_control->reset_rate_controller_I_terms();
-        attitude_control->set_yaw_target_to_current_heading();
-
-        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
-        pos_control->relax_alt_hold_controllers(0.0f);   // forces throttle output to go to zero
-        pos_control->update_z_controller();
-
-        // backstepping
-        backstepping->reset_integral();
-
-        break;
-
-    case AltHold_Flying:
-        motors->set_desired_spool_state(AP_Motors::DESIRED_THROTTLE_UNLIMITED);
-
-        // call attitude controller
-        attitude_control->input_euler_angle_roll_pitch_euler_rate_yaw(target_roll, target_pitch, target_yaw_rate);
-
-        // call backstepping controller
-        backstepping->get_target_pos(0, g.BS_altd); // TODO
-        backstepping->pos_update();
-
-
-        backstepping->update_alt_controller();
-
-        // get control outputs
-        pos_sensor.read_controller(backstepping->perr, backstepping->get_u1());
-        pos_sensor.data.AC_alt_target = g.BS_altd;
-
-        break;
-    }
 }
