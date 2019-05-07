7bb1cc4b368b99a80de5e44e482d162c0bd6f334
calvinl-l-l <calvongchi@gmail.com>
2019-05-07 18:33:45 +1000

    PID: bug fixed for PID tunnel mode

--- ArduCopter/mode_tunnelpid.cpp
+++ ArduCopter/mode_tunnelpid.cpp
@@ -60,9 +60,11 @@ void Copter::ModeTunnelPID::run()
     // generate waypoint
     pp.get_default_target(g.BS_yd, g.BS_zd);
 
+    pp.get_current_pos(pkf->get_pos());
+
     //position_t target_pos = pp.run_circular_trajectory();
-    //position_t target_pos = pp.run_diagonal_trajectory();
-    position_t target_pos = pp.run_setpoint();
+    position_t target_pos = pp.run_diagonal_trajectory();
+    //position_t target_pos = pp.run_setpoint();
 
     // update position and position target
     backstepping->get_target_pos(target_pos.y, target_pos.z);
